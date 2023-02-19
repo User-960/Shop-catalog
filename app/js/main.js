99d4058d5ce843ab1d019cdc67dc43d3e2833548
Ivan <ivanvolodin960@gmail.com>
2023-02-20 00:53:31 +0300

    Issue 01.05.01 - Maked logic of uploading data

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+function render() {
+  headerPage.render();
+  productsPage.render();
+};
+
+let CATALOG = [];
+
+fetch('server/catalog.json')
+  .then(result => result.json())
+  .then(data => {
+    CATALOG = data;
+
+    setTimeout(() => {
+      render();
+    }, 1000);
+
+  })
+  .catch(error => {
+    errorPage.render();
+  });
