7ab3c18fe56abe73fd79dcd049c204c2066d1b14
Ivan <ivanvolodin960@gmail.com>
2023-02-20 00:38:11 +0300

    Issue 01.04.01 - Maked design to scrollbar and added all variables for styles

--- app/constants/root.js
+++ app/constants/root.js
@@ -1,3 +1,4 @@
+/* Root elements */
 const ROOT_HEADER = document.querySelector('.header');
 const ROOT_PRODUCTS = document.querySelector('.products');
 const ROOT_SHOPPING = document.querySelector('.shopping');
