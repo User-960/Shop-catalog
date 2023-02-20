d602c94f65427ffd55410be8c22bb3bcf892edfa
Ivan <ivanvolodin960@gmail.com>
2023-02-20 15:54:04 +0300

    Issue 07.03.01 - Added logo and styles for logo in element Header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -7,6 +7,10 @@ class Header {
     const html = `
       <div class="container">
         <div class="header__inner">
+          <div class="logo">
+            <img class="logo__img" src="../../images/logo-catalog.png" alt="logo"/>
+            <span class="logo__title">Guitar Shop</span>
+          </div>
           <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
             <i class="bi bi-basket2"></i>
             <span class="header-counter__number">${count}</span>
