6341f582a2196a367743ed522ec6b4254477e76f
Ivan <ivanvolodin960@gmail.com>
2023-03-09 00:56:24 +0300

    Issue 08.01.01 - remake address for request

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@ spinnerPage.render();
 
 let CATALOG = [];
 
-fetch('https://gist.githubusercontent.com/User-960/be3152f2020d8b4c9b4c946e7e3a9e30/raw/90e888064e91243e577d216bb817dd717d7eb102/catalog.json')
+fetch('server/catalog.json')
   .then(result => result.json())
   .then(data => {
     CATALOG = data;
